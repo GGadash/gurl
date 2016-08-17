d53530060cbd46f482a3e67d9b63cfff91f54a62
GGadash <akiladj@gmail.com>
2016-08-17 20:31:19 +0530

    Micro Change in Redirecting
    Micro Change in Redirecting. Eloquent used, view removed

--- app/Http/Controllers/UrlredirController.php
+++ app/Http/Controllers/UrlredirController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\UrlMapping;
+use Redirect;
 
 class UrlredirController extends Controller
 {
     public function redirUrl ($id) {
 	
-	return view('urlredir', ['id' => $id]);
+	//$urlredir = UrlMapping::where('id', $id)->first();
+	//return Redirect::away($urlredir->longurl);
 	
+	return Redirect::away(UrlMapping::where('id', $id)->first()->longurl);
+
 	}
 }
